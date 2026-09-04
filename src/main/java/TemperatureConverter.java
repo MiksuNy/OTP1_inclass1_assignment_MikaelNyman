ac9295ff122a966c1d2ee73b2ae431cfe7bc2536
Mikael <miksunyman1000@gmail.com>
2026-09-04 11:41:36 +0300

    Initial commit

---
+++ src/main/java/TemperatureConverter.java
@@ -0,0 +1,13 @@
+public class TemperatureConverter {
+    public static double fahrenheitToCelsius(double fahrenheit) {
+        return (fahrenheit - 32.0) * 5.0 / 9.0;
+    }
+
+    public static double celsiusToFahrenheit(double celsius) {
+        return (celsius * 9.0 / 5.0) + 32.0;
+    }
+    
+    public static boolean isExtremeTemperature(double celsius) {
+        return celsius < -40.0 || celsius > 50.0;
+    }
+}
