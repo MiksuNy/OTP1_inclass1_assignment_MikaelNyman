9876814df6343d78a5664bd22b2d843d5558da36
MiksuNy <miksunyman1000@gmail.com>
2026-09-20 14:54:06 +0300

    Change tests

--- src/test/java/TemperatureConverterTest.java
+++ src/test/java/TemperatureConverterTest.java
@@ -36,9 +36,9 @@ class TemperatureConverterTest {
         var tc = new TemperatureConverter();
         double c1 = 500.0;
         double c2 = 30.0;
-        double c3 = -40.0;
+        double c3 = -500.0;
         assertTrue(tc.isExtremeTemperature(c1));
         assertFalse(tc.isExtremeTemperature(c2));
-        assertFalse(tc.isExtremeTemperature(c3));
+        assertTrue(tc.isExtremeTemperature(c3));
     }
 }
